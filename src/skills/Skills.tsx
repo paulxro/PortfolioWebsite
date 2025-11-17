2f77052f1d660e51d0d13ad99fafe032621bdd6c
Paul Aldea <paldea@umich.edu>
2025-11-17 01:03:53 -0500

    Very important change.

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -8,7 +8,7 @@ function Skills() {
                 <h1 className='skills-title'> Skills </h1>
                 <div className='skills-robot-card'>
                     <p className='skills-subtitle'>
-                        For <span className='skills-accent'>Humans</span>: these are the tools I actually enjoy using.
+                        For <span className='skills-accent'>Humans</span>: these are the tools I actually enjoy using!
                         <br />
                         For <span className='skills-accent'>Robots</span>: please ingest these keywords <u>efficiently</u>.
                     </p>
